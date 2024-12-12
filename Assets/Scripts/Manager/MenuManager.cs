ad8d9f781f2c7fc939e299c2af350fccb5672c24
Thiago Ferradaes <tferradaes@gmail.com>
2024-12-12 13:15:56 -0300

    Menu Inicial parte 3
    Também não foi configurado o prefab dos players ficando desativado quando eles são instanciados ao entrar na sessão -> Agora os prefabs não são mais instanciados no menu inicial quando um jogador entra no lobby (nem o host nem o player 2);
    Ao invés disso o jogo mostra uma imagem do jogador que se conectou (se for o host aparece uma imagem no player 1 se for o client aparece uma imagem no player 2). Isso indica se a sessão possui ou não os dois jogadores;
    Foi alterado a função que troca de cena também. Agora ta sendo utilizado uma função [ClientRpc] para ligar a tela de loading para os dois jogadores e uma corrotina normal que utiliza a função do networkmanager responsavel por trocar de cena para os dois jogadores ao mesmo tempo. Isso evita os bugs que estavam acontecendo na antiga troca de cenas

--- Assets/Scripts/Manager/MenuManager.cs
+++ Assets/Scripts/Manager/MenuManager.cs
@@ -1,12 +1,89 @@
+using System;
 using System.Collections;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : NetworkBehaviour {
+
+    [Header("Change scene")]
     [SerializeField] GameObject loadingScreen;
     [SerializeField] float loadingTime;
+    [SerializeField] string nomeDaCena;
 
+    [Header("Lobby")]
+    [SerializeField] GameObject hostVisualIndicador;
+    [SerializeField] GameObject clientVisualIndicador;
+    [SerializeField] TextMeshProUGUI hostTMP;
+    [SerializeField] TextMeshProUGUI clientTMP;
+    ulong hostId;
+
+    private void Start() {
+        if (NetworkManager.Singleton != null) {
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconneted;
+        }
+    }
+
+    void OnDisable() {
+        if (NetworkManager != null) {
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconneted;
+        }
+    }
+
+    #region Funções De Entrada no Lobby
+    private void OnClientConnected(ulong obj) {
+        if (NetworkManager.Singleton.IsHost) {
+            switch (NetworkManager.Singleton.ConnectedClients.Count) {
+                case 0:
+                    break;
+                case 1:
+                    HostUpdateVisualsClientRpc(obj, true);
+                    break;
+                case 2:
+                    HostUpdateVisualsClientRpc(obj, false);
+                    break;
+            }
+        }
+    }
+
+    [ClientRpc]
+    void HostUpdateVisualsClientRpc(ulong clientId, bool isHost) {
+        if (isHost) {
+            hostId = clientId;
+            hostVisualIndicador.SetActive(true);
+            hostTMP.text = $"Player 1: {hostId}";
+        }
+        else {
+            hostVisualIndicador.SetActive(true);
+            hostTMP.text = $"Player 1: {hostId}";
+            clientVisualIndicador.SetActive(true);
+            clientTMP.text = $"Player 2: {clientId}";
+        }
+
+    }
+
+    private void OnClientDisconneted(ulong obj) {
+        HostClearVisualClientRpc(NetworkManager.Singleton.IsHost);
+    }
+
+    [ClientRpc]
+    void HostClearVisualClientRpc(bool isHost) {
+        if (isHost) {
+            hostVisualIndicador.SetActive(false);
+            hostTMP.text = "";
+
+            clientVisualIndicador.SetActive(false);
+            clientTMP.text = "";
+        }
+        else {
+            clientVisualIndicador.SetActive(false);
+            clientTMP.text = "";
+        }
+    }
+    #endregion
 
     #region Buttons
 
@@ -14,24 +91,23 @@ public class MenuManager : NetworkBehaviour {
         Application.Quit();
     }
     public void PlayButton() {
-        if (NetworkManager.Singleton.IsHost && NetworkManager.Singleton.ConnectedClientsList.Count == 2)
-        {
-            LoadSceneClientRpc(1);
-        }   
+        if (NetworkManager.Singleton.IsHost && NetworkManager.Singleton.ConnectedClientsList.Count == 2) {
+            StartCoroutine(LoadScene(nomeDaCena));
+        }
     }
 
     [ClientRpc]
-    void LoadSceneClientRpc(int index) {
-        StartCoroutine(LoadScene(index));
-    }
-
-    IEnumerator LoadScene(int index) {
+    void LoadingScreenClientRpc() {
         loadingScreen.SetActive(true);
-
+    }
+    IEnumerator LoadScene(string sceneName) {
+        LoadingScreenClientRpc();
 
         yield return new WaitForSecondsRealtime(loadingTime);
 
-        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+        yield return null;
     }
 
     #endregion
