40ca35945971616aaf0cf5f43648bbb4e8e3f176
Thiago Ferradaes <tferradaes@gmail.com>
2024-12-18 18:38:56 -0300

    Camera parte 1
    Cinemachine instalado;
    Camera Follow criada e colocada na cena;
    Agora quando a cena começa a camera vai ser designada localmente para seguir um dos personagens, depende qual personagem o player é;
    Alterações feitas na função de distribuição do ownership dos personagens na cena -> Agora é uma corrotina que espera os dois players estarem conectados antes de distribuir o ownership, isso evitou que o player 1 (host) tenha controle temporario dos dois personagens;
    Alteração na fórmula de movimentação dos players também.

--- Assets/Scripts/Player/Controllers/PlayerController.cs
+++ Assets/Scripts/Player/Controllers/PlayerController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using Unity.Cinemachine;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,52 +10,72 @@ public class PlayerController : NetworkBehaviour {
 
     [Header("Ownership")]
     [SerializeField] private Characters character;
+    NetworkObject _netWorkObject;
+
+    [Header("Camera")]
+    [SerializeField] private GameObject shoulder;
+    [SerializeField] CinemachineCamera cameraOverTheShoulder;
 
     [Header("Movement")]
     public float CharacterMoveSpeed;
     void Start() {
+        _netWorkObject = GetComponent<NetworkObject>();
+
         if (NetworkManager.Singleton.IsHost) {
-            NetworkManager.SceneManager.OnLoadComplete += DistributeOwnership;
+            StartCoroutine(nameof(DistributeOwnership));
+        }
+
+        StartCoroutine(nameof(WaitToSetCamera));
+    }
+    IEnumerator WaitToSetCamera() {
+        while (!_netWorkObject.IsOwner) {
+            yield return null;
+        }
+        Debug.Log(_netWorkObject.name);
+        SetFollowCamera();
+    }
+
+    private void SetFollowCamera() {
+        if (NetworkManager.Singleton.LocalClientId == _netWorkObject.OwnerClientId) {
+            cameraOverTheShoulder.Follow = shoulder.transform;
         }
     }
 
-    private void DistributeOwnership(ulong clientId, string sceneName, LoadSceneMode loadSceneMode) {
-        Debug.Log("Entrei aqui");
-        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2) {
-            NetworkObject obj = GetComponent<NetworkObject>();
-            Debug.Log("Jogadores conectados");
-            if (WhiteBoard.Singleton.PlayerOneCharacter.Value == character) {
-                obj.ChangeOwnership(NetworkManager.Singleton.ConnectedClientsList[0].ClientId);
-                Debug.Log("Player One: " + NetworkManager.Singleton.ConnectedClientsList[0].ClientId
-                    + WhiteBoard.Singleton.PlayerOneCharacter.Value);
-            }
+    private IEnumerator DistributeOwnership() {
 
-            if (WhiteBoard.Singleton.PlayerTwoCharacter.Value == character) {
-                obj.ChangeOwnership(NetworkManager.Singleton.ConnectedClientsList[1].ClientId);
-                Debug.Log("Player Two: " + NetworkManager.Singleton.ConnectedClientsList[1].ClientId
-                    + WhiteBoard.Singleton.PlayerTwoCharacter.Value);
-            }
-
-            NetworkManager.SceneManager.OnLoadComplete -= DistributeOwnership;
+        while (NetworkManager.Singleton.ConnectedClientsList.Count < 2) {
+            yield return null;
+            Debug.Log("Waiting");
         }
+
+        if (WhiteBoard.Singleton.PlayerOneCharacter.Value == character) {
+            _netWorkObject.ChangeOwnership(NetworkManager.Singleton.ConnectedClientsList[0].ClientId);
+            Debug.Log("Player 1: " + WhiteBoard.Singleton.PlayerOneCharacter.Value);
+        }
+        else if (WhiteBoard.Singleton.PlayerTwoCharacter.Value == character) {
+            _netWorkObject.ChangeOwnership(NetworkManager.Singleton.ConnectedClientsList[1].ClientId);
+            Debug.Log("Player 2: " + WhiteBoard.Singleton.PlayerTwoCharacter.Value);
+        }
+
+
     }
 
     void FixedUpdate() {
-            MovementInputs();
+        MovementInputs();
     }
 
     private void MovementInputs() {
         if (Keyboard.current.wKey.isPressed) {
-            transform.position += new Vector3(0, 0, CharacterMoveSpeed);
+            transform.Translate(CharacterMoveSpeed * Time.deltaTime * Vector3.forward.normalized);
         }
         else if (Keyboard.current.sKey.isPressed) {
-            transform.position += new Vector3(0, 0, -CharacterMoveSpeed);
+            transform.Translate(CharacterMoveSpeed * Time.deltaTime * Vector3.back.normalized);
         }
         else if (Keyboard.current.dKey.isPressed) {
-            transform.position += new Vector3(CharacterMoveSpeed, 0, 0);
+            transform.Translate(CharacterMoveSpeed * Time.deltaTime * Vector3.right.normalized);
         }
         else if (Keyboard.current.aKey.isPressed) {
-            transform.position += new Vector3(-CharacterMoveSpeed, 0, 0);
+            transform.Translate(CharacterMoveSpeed * Time.deltaTime * Vector3.left.normalized);
         }
     }
 }
