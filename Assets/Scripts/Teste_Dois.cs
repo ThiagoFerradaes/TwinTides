8aedf80d810363add0f1df2659d08340ca0abdab
Thiago Ferradaes <tferradaes@gmail.com>
2024-12-22 16:37:47 -0300

    Sistema de Inputs 2
    Criação do input para o dash;
    Criação de funções para os inputs:
    - Jump
    - Sprint
    - Dash
    DeadZone da camera removido por que não estava funcionando bem com a nova movimentação do jogador;

--- Assets/Scripts/Teste_Dois.cs
+++ Assets/Scripts/Teste_Dois.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Threading;
 using Unity.Cinemachine;
 using UnityEngine;
@@ -5,12 +6,32 @@ using UnityEngine.InputSystem;
 
 public class Teste_Dois : MonoBehaviour
 {
-    // Movimento
+    [Header("Movimento")]
     public float CharacterMoveSpeed;
+    public float SprintSpeed;
     public float RotationSpeed;
+    bool _canWalk;
+
+    [Header("Pulo")]
+    public float JumpForce;
+    public LayerMask FloorLayer;
+    public int MaxJumps;
+    public float FallForce;
+
+    [Header("Dash")]
+    public float DashForce;
+    public float DashCooldown;
+    public float DashDuration;
+    bool _inDash;
+
+
+    float _currentCharacterMoveSpeed;
     float _rotationY;
     Vector2 _moveInput;
     Vector2 _rotationInput;
+    
+    int _currentJumpsAlowed;
+    Rigidbody rb;
 
     // Camera
     public CinemachineCamera Camera;
@@ -21,10 +42,13 @@ public class Teste_Dois : MonoBehaviour
     private void Start() {
         _cameraInputController = Camera.GetComponent<CinemachineInputAxisController>();
         _cameraOrbital = Camera.GetComponent<CinemachineOrbitalFollow>();
+        rb = GetComponent<Rigidbody>();
+
+        _currentCharacterMoveSpeed = CharacterMoveSpeed;
     }
 
     public void InputMove(InputAction.CallbackContext context) {
-        if (context.phase == InputActionPhase.Performed) 
+        if (context.phase == InputActionPhase.Performed && _canWalk) 
     {
             _moveInput = context.ReadValue<Vector2>();
         }
@@ -41,13 +65,55 @@ public class Teste_Dois : MonoBehaviour
             _rotationInput = Vector2.zero;
         }
     }
+    public void InputJump(InputAction.CallbackContext context) {
+        if (context.phase == InputActionPhase.Performed && _currentJumpsAlowed > 0) {
+            _currentJumpsAlowed--;
+            rb.linearVelocity = new(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
+    public void InputSprint(InputAction.CallbackContext context) {
+        if (context.phase == InputActionPhase.Started) {
+            _currentCharacterMoveSpeed = SprintSpeed;
+        }
+        else if (context.phase == InputActionPhase.Canceled){
+            _currentCharacterMoveSpeed = CharacterMoveSpeed;
+        }
+    }
+    public void InputDash(InputAction.CallbackContext context) {
+        if (context.phase == InputActionPhase.Performed && !_inDash) {
+            StartCoroutine(DashCoroutine());
+        }
+    }
 
+    IEnumerator DashCoroutine() {
+        _inDash = true;
+        _canWalk = false;
+        float startTime = Time.time;
 
+        while(Time.time - startTime < DashDuration) {
+            if (_moveInput.magnitude >= 0.1f) {
+                Vector3 moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y).normalized;
+                transform.Translate(DashForce * Time.deltaTime * moveDirection);
+                yield return null;
+            }
+            else {
+                transform.Translate(DashForce * Time.deltaTime * Vector3.forward);
+                yield return null;
+            }
+        }
+
+        _canWalk = true;
+
+        yield return new WaitForSeconds(DashCooldown);
+        _inDash = false;
+
+    }
     private void FixedUpdate() {
         if (_moveInput.magnitude >= 0.1f) {
             // Movimentação
             Vector3 moveDirection = new Vector3(_moveInput.x, 0, _moveInput.y).normalized;
-            transform.Translate(CharacterMoveSpeed * Time.deltaTime * moveDirection);
+            transform.Translate(_currentCharacterMoveSpeed * Time.deltaTime * moveDirection);
 
             // Rotação de camera
             _cameraInputController.enabled = false;
@@ -59,6 +125,18 @@ public class Teste_Dois : MonoBehaviour
         }
         else {
             _cameraInputController.enabled = true;
+        }
+
+        
+        if (rb.linearVelocity.y != 0) { // Durante o pulo aumenta a gravidade, serve para regular a duração do pulo
+            rb.AddForce(Vector3.down * FallForce, ForceMode.Acceleration);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (((1 << collision.gameObject.layer) & FloorLayer.value) != 0) {
+            _currentJumpsAlowed = MaxJumps;
         }
     }
 }
