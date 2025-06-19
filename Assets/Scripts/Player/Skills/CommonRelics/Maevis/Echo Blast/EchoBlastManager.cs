f5b78f2824c11010a21228a7ac90a45f42eafd8c
Thiago Ferradaes <139820739+ThiagoFerradaes@users.noreply.github.com>
2025-06-19 00:12:51 -0300

    Animações
    Animação do echoblast

--- Assets/Scripts/Player/Skills/CommonRelics/Maevis/Echo Blast/EchoBlastManager.cs
+++ Assets/Scripts/Player/Skills/CommonRelics/Maevis/Echo Blast/EchoBlastManager.cs
@@ -7,6 +7,7 @@ public class EchoBlastManager : SkillObjectPrefab {
     SkillContext _context;
     GameObject _maevis;
     Animator anim;
+    PlayerController _pController;
 
     public override void ActivateSkill(Skill info, int skillLevel, SkillContext context) {
         _info = info as EchoBlast;
@@ -16,22 +17,49 @@ public class EchoBlastManager : SkillObjectPrefab {
         if (_maevis == null) {
             _maevis = PlayerSkillPooling.Instance.MaevisGameObject;
             anim = _maevis.GetComponentInChildren<Animator>();
+            _pController = _maevis.GetComponentInChildren<PlayerController>();
         }
 
         EchoBlastStunExplosion.OnSecondaryExplosion += EchoBlastStunExplosion_OnSecondaryExplosion;
 
         gameObject.SetActive(true);
 
+        StartCoroutine(AttackRoutine());
+
+        StartCoroutine(Duration());
+    }
+
+    IEnumerator AttackRoutine() {
+        _pController.BlockMovement();
+
         anim.SetTrigger("EchoBlast");
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        while (anim.IsInTransition(0)) yield return null;
+
+        while (stateInfo.IsName(_info.AnimationName) == false) {
+            yield return null;
+            stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        }
+
+        while (stateInfo.normalizedTime < _info.AnimationPercentToAttack) {
+            yield return null;
+            stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        }
 
         if (LocalWhiteBoard.Instance.PlayerCharacter == Characters.Maevis) {
             int skillId = PlayerSkillConverter.Instance.TransformSkillInInt(_info);
             PlayerSkillPooling.Instance.RequestInstantiateRpc(skillId, _context, _level, 1);
         }
 
-        StartCoroutine(Duration());
-    }
+        while (stateInfo.normalizedTime < 1) {
+            yield return null;
+            stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        }
 
+        _pController.AllowMovement();
+    }
     private void EchoBlastStunExplosion_OnSecondaryExplosion(object sender, EchoBlastStunExplosion.ExplosionPosition e) {
         StartCoroutine(SecondaryExplosion(e.context));
     }
@@ -50,5 +78,10 @@ public class EchoBlastManager : SkillObjectPrefab {
                 PlayerSkillPooling.Instance.RequestInstantiateRpc(skillId, context, _level, 3);
             }
         }
+    }
+
+    public override void ReturnObject() {
+        _pController.AllowMovement();
+        base.ReturnObject();
     }
 }
